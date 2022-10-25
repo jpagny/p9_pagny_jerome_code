6c5ed16cb38aa9b1e759e91fb55d77dd73b95ea2
Jérôme Pagny <pagny.jerome@gmail.com>
2022-10-25 12:36:29 +0200

    Add org.springdoc in Note module + add docs api

--- note/src/main/java/com/mediscreen/note/controller/NoteController.java
+++ note/src/main/java/com/mediscreen/note/controller/NoteController.java
@@ -3,6 +3,13 @@ package com.mediscreen.note.controller;
 import com.mediscreen.note.dto.NoteDTO;
 import com.mediscreen.note.exception.ResourceNotFoundException;
 import com.mediscreen.note.service.impliment.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/note")
+@Tag(name = "note", description = "the Note API")
 public class NoteController {
 
     private final NoteService noteService;
@@ -19,7 +27,12 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    @GetMapping("/list")
+    @GetMapping(value = "/list", produces = {"application/json"})
+    @Operation(summary = "Find all notes", description = "Also returns a link to retrieve all notes")
+    @ApiResponse(responseCode = "200",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = NoteDTO.class))))
+    @ApiResponse(responseCode = "204", description = "No Content",
+            content = @Content())
     public ResponseEntity<List<NoteDTO>> getAll() {
         List<NoteDTO> notes = noteService.getAll();
 
@@ -30,8 +43,15 @@ public class NoteController {
         return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
-    @GetMapping("/{patientId}/list")
-    public ResponseEntity<List<NoteDTO>> getAll(@PathVariable("patientId") Long patientId) {
+    @GetMapping(value = "/{patientId}/list", produces = {"application/json"})
+    @Operation(summary = "Find all notes by patient id", description = "Also returns a link to retrieve all notes from a patient")
+    @ApiResponse(responseCode = "200",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = NoteDTO.class))))
+    @ApiResponse(responseCode = "204", description = "No Content",
+            content = @Content())
+    public ResponseEntity<List<NoteDTO>> getAll(
+            @Parameter(description = "Long patientId.", required = true)
+            @PathVariable("patientId") Long patientId) {
         List<NoteDTO> notes = noteService.getAllByPatientId(patientId);
 
         if (notes.isEmpty()) {
@@ -41,8 +61,15 @@ public class NoteController {
         return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<NoteDTO> get(@PathVariable("id") String id) {
+    @GetMapping(value = "/{id}", produces = {"application/json"})
+    @Operation(summary = "Find a note by id", description = "Also returns a link to note information")
+    @ApiResponse(responseCode = "200", description = "Note fetched",
+            content = @Content(schema = @Schema(implementation = NoteDTO.class)))
+    @ApiResponse(responseCode = "404", description = "No found",
+            content = @Content())
+    public ResponseEntity<NoteDTO> get(
+            @Parameter(description = "String id.", required = true)
+            @PathVariable("id") String id) {
         try {
             NoteDTO note = noteService.get(id);
             return new ResponseEntity<>(note, HttpStatus.OK);
@@ -51,8 +78,15 @@ public class NoteController {
         }
     }
 
-    @PutMapping("/")
-    public ResponseEntity<NoteDTO> update(@RequestBody NoteDTO note) {
+    @PutMapping(value = "/", produces = {"application/json"})
+    @Operation(summary = "Update a note", description = "Also returns a link to note updated")
+    @ApiResponse(responseCode = "200", description = "Note updated",
+            content = @Content(schema = @Schema(implementation = NoteDTO.class)))
+    @ApiResponse(responseCode = "404", description = "No found",
+            content = @Content())
+    public ResponseEntity<NoteDTO> update(
+            @Parameter(description = "NoteDTO note.", required = true, schema = @Schema(implementation = NoteDTO.class))
+            @RequestBody NoteDTO note) {
         try {
             NoteDTO noteUpdated = noteService.update(note);
             return new ResponseEntity<>(noteUpdated, HttpStatus.OK);
@@ -61,14 +95,26 @@ public class NoteController {
         }
     }
 
-    @PostMapping("/")
-    public ResponseEntity<NoteDTO> create(@RequestBody NoteDTO note) {
+    @PostMapping(value = "/", produces = {"application/json"})
+    @Operation(summary = "Create a note", description = "Also returns a link to note created")
+    @ApiResponse(responseCode = "200", description = "Patient created",
+            content = @Content(schema = @Schema(implementation = NoteDTO.class)))
+    public ResponseEntity<NoteDTO> create(
+            @Parameter(description = "NoteDTO note.", required = true, schema = @Schema(implementation = NoteDTO.class))
+            @RequestBody NoteDTO note) {
         NoteDTO noteCreated = noteService.create(note);
         return new ResponseEntity<>(noteCreated, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") String id) {
+    @DeleteMapping(value = "/{id}", produces = {"application/json"})
+    @Operation(summary = "Delete a note", description = "Patient will be note")
+    @ApiResponse(responseCode = "200", description = "Note deleted",
+            content = @Content())
+    @ApiResponse(responseCode = "404", description = "Not found",
+            content = @Content())
+    public ResponseEntity<String> delete(
+            @Parameter(description = "String id.", required = true)
+            @PathVariable("id") String id) {
         try {
             noteService.delete(id);
             return new ResponseEntity<>(HttpStatus.OK);
