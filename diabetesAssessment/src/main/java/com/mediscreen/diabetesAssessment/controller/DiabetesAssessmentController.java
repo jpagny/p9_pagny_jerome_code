75522e11579091906bccad2887368b7c56c48bd0
Jérôme Pagny <pagny.jerome@gmail.com>
2022-10-27 19:37:59 +0200

    Add documentation in Module Diabetes assessment

--- diabetesAssessment/src/main/java/com/mediscreen/diabetesAssessment/controller/DiabetesAssessmentController.java
+++ diabetesAssessment/src/main/java/com/mediscreen/diabetesAssessment/controller/DiabetesAssessmentController.java
@@ -3,6 +3,12 @@ package com.mediscreen.diabetesAssessment.controller;
 import com.mediscreen.diabetesAssessment.bean.PatientBean;
 import com.mediscreen.diabetesAssessment.dto.DiabetesAssessmentDTO;
 import com.mediscreen.diabetesAssessment.service.impliment.DiabetesAssessmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +26,21 @@ public class DiabetesAssessmentController {
 
     private final DiabetesAssessmentService assessmentService;
 
-    @GetMapping("/{patientId}")
-    public ResponseEntity<DiabetesAssessmentDTO> getAssessmentByPatientID(@PathVariable("patientId") Long patientId) {
+    @GetMapping(value = "/{patientId}", produces = {"application/json"})
+    @Operation(summary = "Find the diabetes assessment by a patient", description = "Also returns a link to retrieve the diabetes assessment")
+    @ApiResponse(responseCode = "200",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = DiabetesAssessmentDTO.class))))
+    public ResponseEntity<DiabetesAssessmentDTO> getAssessmentByPatientID(
+            @Parameter(description = "Long patientId.", required = true)
+            @PathVariable("patientId") Long patientId) {
         DiabetesAssessmentDTO assessment = assessmentService.getAssessmentByPatient(patientId);
         return new ResponseEntity<>(assessment, HttpStatus.OK);
     }
 
-    @GetMapping("/list/{riskLevel}")
+    @GetMapping(value = "/list/{riskLevel}", produces = {"application/json"})
+    @Operation(summary = "Find all notes", description = "Also returns a link to retrieve all patient order by risk level")
+    @ApiResponse(responseCode = "200",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = PatientBean.class))))
     public ResponseEntity<List<PatientBean>> getAssessmentByRiskLevel(@PathVariable("riskLevel") String riskLevel) {
         List<PatientBean> listPatients = assessmentService.getPatientByRiskLevel(riskLevel);
         return new ResponseEntity<>(listPatients, HttpStatus.OK);
